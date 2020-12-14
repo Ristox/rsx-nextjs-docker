6417dd68e2db88a65e501e5746cfe607b5f73dd7
Risto Uibo <risto.uibo@ignite.ee>
2020-12-14 16:46:22 +0200

    backend.ts: use "trackingClient" to register app startup events + refactor/simplify server setup

--- backend.ts
+++ backend.ts
@@ -1,43 +1,35 @@
-import * as dotenv from 'dotenv';
 import * as express from 'express';
 
 import * as next from 'next';
-import * as appInsights from 'applicationinsights';
 
 import { api } from './server';
+import { trackingClient } from "./server/tracking";
 
-dotenv.config();
+
+trackingClient.trackEvent({name: 'Startup...'});
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const nextApp = next({ dev });
 
-app.prepare()
-  .then(() => {
-    const port = process.env.PORT || 3000;
-    const aiKey = process.env.APP_INSIGHTS;
+const startServer = () => {
+  const port = process.env.PORT || 3000;
 
-    const server = express();
-    server.use('/api', api);
+  const server = express();
+  server.use('/api', api);
+  server.get('/p/:id', (req, res) => nextApp.render(req, res, '/post', req.params));
+  server.get('*', (req, res) => nextApp.handleRequest(req, res));
 
-    server.get('/p/:id', (req, res) => {
-      const actualPage = '/post'
-      const queryParams = { id: req.params.id }
-      app.render(req, res, actualPage, queryParams)
-    });
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    });
+  server.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Ready on http://localhost:${port}`);
+    console.log(`Using AppInsights Tracking: ${trackingClient.config}`);
+    trackingClient.trackEvent({name: 'Startup Complete.'});
+  });
+};
 
-    server.listen(port, (err) => {
-      if (err) {
-         throw err;
-      }
-      console.log(`Ready on http://localhost:${port}`);
-
-      console.log(`Using AppInsights: ${aiKey}`);
-      appInsights.setup(aiKey).start();
-    });
-  })
+nextApp.prepare()
+  .then(startServer)
   .catch((ex) => {
     console.error(ex.stack);
     process.exit(1);
