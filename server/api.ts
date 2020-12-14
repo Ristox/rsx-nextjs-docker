adab5157885852cb8c2e0ee12580eb53beb67b26
Risto Uibo <risto.uibo@ignite.ee>
2020-12-14 16:47:13 +0200

    server/api: add custom events tracking for all main server actions

--- server/api.ts
+++ server/api.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 
 import { Actions } from './actions';
+import { trackingClient } from './tracking';
 
 const actions = new Actions();
 
@@ -9,7 +10,9 @@ const api = express.Router();
 api.get('/delay/:ms', async (req:any, res:any) => {
   const { ms } = req.params;
 
+  trackingClient.trackEvent({name: 'Delay - Request', properties: {requestDelay: ms}});
   const delay = await actions.delayFor(ms);
+  trackingClient.trackEvent({name: 'Delay - Actual', properties: {delay}});
 
   res.send(`Done delaying for ${delay} ms`);
 });
@@ -17,13 +20,16 @@ api.get('/delay/:ms', async (req:any, res:any) => {
 api.get('/cpu-load-during/:ms', async (req:any, res:any) => {
   const { ms } = req.params;
 
+  trackingClient.trackEvent({name: 'CPU Load - Request', properties: {requestDelay: ms}});
   const delay = await actions.cpuLoadFor(ms);
+  trackingClient.trackEvent({name: 'CPU Load - Actual', properties: {delayInSeconds: delay}});
 
   res.send(`Done simulating CPU load for about ${delay} seconds`);
 });
 
 api.get('/hello', async (req:any, res:any) => {
   const response = await actions.hello();
+  trackingClient.trackEvent({name: 'Hello Call - Response', properties: {response}});
 
   res.send(response);
 });
